1d0cbfaec169de1d7bcc97d77102cffd9bb00d9c
Joel Helbling <joel@joelhelbling.com>
2010-10-31 11:25:36 -0400

    Initial commit of the Really Test Framework (developed with JoshWalsh)

---
+++ really.rb
@@ -0,0 +1,11 @@
+class FalseClass
+  def really?
+    fail "No way, dude."
+  end
+end
+
+class TrueClass
+  def really?
+    puts "Yeah, dude, like totally"
+  end
+end
---
+++ really_really.rb
@@ -0,0 +1,10 @@
+# raise exception on false.really?
+begin
+  (true == false).really?
+rescue Exception => e
+  (e.message == "No way, dude.").really?
+end
+
+# give a merit badge on true.really?
+(true == true).really?
+
